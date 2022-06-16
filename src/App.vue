c33e4c6f54677f5ac6c62e114a9ad97bf5543d9d
Hayden Bradfield <hbradfield20@gmail.com>
2022-06-16 11:47:03 -0400

    wip

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,33 @@
 </template>
 
 <script>
-
+  export default {
+    name: 'app',
+    data(){
+      return {
+        isLoading: true, 
+        hasLoadError: false
+      }
+    }, 
+    mounted(){
+      this.$store.dispatch('fetchUserData')
+      .then(result => {
+        console.log('result', result);
+      })
+      .catch(e => {
+        console.log(e);
+        this.hasLoadError = true;
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+    },
+    computed: {
+      userData(){
+        return this.$store.getters['userData'];
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,42 @@ const store = createStore({
     strict: true, 
     state(){
         return {
-
+            userData: []
         }
     }, 
-    getters: {}, 
-    mutations: {}, 
-    actions: {}
+    getters: {
+        userData(state){
+            return state.userData;
+        }
+    }, 
+    mutations: {
+        storeUserData(state, payload){
+            state.userData = payload;
+        }
+    }, 
+    actions: {
+        async fetchUserData({commit}){
+
+            try{
+
+                const resp = await fetch('https://jsonplaceholder.typicode.com/users');
+                if(!resp.ok){
+                    throw new Error('Response failed.');
+                }
+                const data = await resp.json();
+
+                console.log(data);
+                commit('storeUserData', data);
+
+                return true;
+            
+            }catch(e){
+                console.log(e);
+                throw new Error(e);
+            }
+            
+        }
+    }
 });
 
 
