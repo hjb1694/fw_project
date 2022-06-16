42afd6c1f82b6b2e5e741e2bdc1e05f93e435050
Hayden Bradfield <hbradfield20@gmail.com>
2022-06-16 11:22:08 -0400

    create store

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+    strict: true, 
+    state(){
+        return {
+
+        }
+    }, 
+    getters: {}, 
+    mutations: {}, 
+    actions: {}
+});
+
+
+export default store;
